60ebc4a76e761a1e2630ac713b56ff8d987c6e31
shawnm4 <shawnm18057310@gmail.com>
2024-03-06 22:29:40 -0500

    deleted config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routing/routes";
-import { ConfigProvider } from "antd";
 
 export default function App() {
   return <RouterProvider router={createBrowserRouter(routes)} />;
